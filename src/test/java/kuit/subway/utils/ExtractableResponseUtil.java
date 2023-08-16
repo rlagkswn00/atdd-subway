a063f652139418c0f5349a06c0c5b8f12943ebb0
Han Ju Kim <hjkim2902@gmail.com>
2023-08-16 21:26:37 +0900

    feat : 요청,응답 util 클래스 추가

---
+++ src/test/java/kuit/subway/utils/ExtractableResponseUtil.java
@@ -0,0 +1,20 @@
+package kuit.subway.utils;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+public class ExtractableResponseUtil {
+
+    public static ExtractableResponse<Response> post(String path, Object body) {
+        return RestAssured.given().log().all()
+                .contentType(ContentType.JSON) // 추가 해야 합니다!
+                .body(body)
+                .when().post(path)
+                .then().log().all()
+                .extract();
+    }
+}
