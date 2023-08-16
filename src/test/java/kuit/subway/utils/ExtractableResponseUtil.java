8a12e8224f437499c7936c759a53217af32f28bc
Han Ju Kim <hjkim2902@gmail.com>
2023-08-16 21:29:21 +0900

    feat : 요청/응답 Util에 GET 메소드 추가

--- src/test/java/kuit/subway/utils/ExtractableResponseUtil.java
+++ src/test/java/kuit/subway/utils/ExtractableResponseUtil.java
@@ -8,12 +8,18 @@ import io.restassured.response.Response;
 import java.util.Map;
 
 public class ExtractableResponseUtil {
-
     public static ExtractableResponse<Response> post(String path, Object body) {
         return RestAssured.given().log().all()
                 .contentType(ContentType.JSON) // 추가 해야 합니다!
                 .body(body)
                 .when().post(path)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> get(String path) {
+        return RestAssured.given().log().all()
+                .when().get(path)
                 .then().log().all()
                 .extract();
     }
