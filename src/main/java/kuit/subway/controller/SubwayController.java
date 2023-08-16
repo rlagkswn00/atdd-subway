53ec12875f7b2a3b5142682ebf98417987c40df6
Han Ju Kim <hjkim2902@gmail.com>
2023-08-16 21:50:39 +0900

    feat : 지하철 역 조회 API 생성

--- src/main/java/kuit/subway/controller/SubwayController.java
+++ src/main/java/kuit/subway/controller/SubwayController.java
@@ -1,5 +1,6 @@
 package kuit.subway.controller;
 
+import kuit.subway.dto.FindStationsRes;
 import kuit.subway.dto.SaveStationReq;
 import kuit.subway.dto.SaveStationRes;
 import kuit.subway.service.StationService;
@@ -7,21 +8,28 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/station")
 public class SubwayController {
     private final StationService stationService;
 
-    @PostMapping
+    @PostMapping("/station")
     public ResponseEntity<SaveStationRes> createStation(@RequestBody SaveStationReq stationReq) {
-        log.info("API 실행");
         SaveStationRes saveStationRes = stationService.createStation(stationReq);
         return ResponseEntity.ok(saveStationRes);
+    }
+
+    @GetMapping("/stations")
+    public ResponseEntity<List<FindStationsRes>> findStations() {
+        List<FindStationsRes> findStations = stationService.findStations();
+        return ResponseEntity.ok(findStations);
     }
 }
--- src/main/java/kuit/subway/service/StationServiceImpl.java
+++ src/main/java/kuit/subway/service/StationServiceImpl.java
@@ -2,12 +2,15 @@ package kuit.subway.service;
 
 import jakarta.transaction.Transactional;
 import kuit.subway.domain.Station;
+import kuit.subway.dto.FindStationsRes;
 import kuit.subway.dto.SaveStationReq;
 import kuit.subway.dto.SaveStationRes;
 import kuit.subway.repository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -16,12 +19,23 @@ import org.springframework.stereotype.Service;
 public class StationServiceImpl implements StationService {
     private final StationRepository stationRepository;
 
+    @Override
     public SaveStationRes createStation(SaveStationReq saveStationReq) {
-        log.info("service 실행");
         Station station = Station.builder()
                 .name(saveStationReq.getName())
                 .build();
         Long id = stationRepository.save(station).getId();
         return new SaveStationRes(id);
+    }
+
+    @Override
+    public List<FindStationsRes> findStations() {
+        List<FindStationsRes> stations = stationRepository.findAll().stream()
+                .map(s -> FindStationsRes.builder()
+                        .id(s.getId())
+                        .name(s.getName()).build())
+                .toList();
+
+        return stations;
     }
 }
