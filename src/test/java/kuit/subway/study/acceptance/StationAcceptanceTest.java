1eb23937ae29effbf40bfa3dad87d44a4873d09b
Han Ju Kim <hjkim2902@gmail.com>
2023-08-16 21:27:21 +0900

    refactor : 역 생성 테스트 시 픽스처 분리 형태로 변경

--- src/test/java/kuit/subway/study/acceptance/StationAcceptanceTest.java
+++ src/test/java/kuit/subway/study/acceptance/StationAcceptanceTest.java
@@ -5,31 +5,35 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kuit.subway.AcceptanceTest;
+import kuit.subway.dto.SaveStationReq;
+import kuit.subway.study.StationFixture;
+import kuit.subway.utils.ExtractableResponseUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static kuit.subway.study.StationFixture.지하철_역_생성_픽스처;
+import static kuit.subway.utils.ExtractableResponseUtil.*;
+
 public class StationAcceptanceTest extends AcceptanceTest {
+
 
     @DisplayName("역 생성 테스트")
     @Test
-    void createStationTest(){
-        Map<String, String> body = new HashMap<>();
-        body.put("name","별내역");
-
-        ExtractableResponse<Response> extract =
-                RestAssured.given().log().all()
-                .contentType(ContentType.JSON) // 추가 해야 합니다!
-                .body(body)
-                .when().post("/station")
-                .then().log().all()
-                .extract();
+    void 지하철_역_생성_테스트() {
+        ExtractableResponse<Response> extract = 지하철_역_생성(지하철_역_생성_픽스처("별내역"));
         //200이면 정상 추가
-        Assertions.assertEquals(200,extract.statusCode());
+        Assertions.assertEquals(200, extract.statusCode());
     }
+
+    private ExtractableResponse<Response> 지하철_역_생성(SaveStationReq saveStationReq){
+        return post("/station", saveStationReq);
+    }
+
+
+
 }
