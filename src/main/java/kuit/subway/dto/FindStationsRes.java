c8b7007859bb1ceea7c001f26727cd7c80493770
Han Ju Kim <hjkim2902@gmail.com>
2023-08-16 21:50:30 +0900

    feat : 지하철 역 조회 DTO 생성

---
+++ src/main/java/kuit/subway/dto/FindStationsRes.java
@@ -0,0 +1,15 @@
+package kuit.subway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FindStationsRes {
+    private Long id;
+    private String name;
+}
