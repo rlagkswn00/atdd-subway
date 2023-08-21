d07836afa29ffb22663003e6f206941bf0674b49
Han Ju Kim <hjkim2902@gmail.com>
2023-08-21 16:44:23 +0900

    refactor : 라인 생성 픽스처 메소드명 변경

--- src/test/java/kuit/subway/study/fixture/LineFixture.java
+++ src/test/java/kuit/subway/study/fixture/LineFixture.java
@@ -1,11 +1,10 @@
 package kuit.subway.study.fixture;
 
-import kuit.subway.domain.Line;
 import kuit.subway.dto.SaveLineReq;
 import kuit.subway.dto.UpdateLineReq;
 
 public class LineFixture {
-    public static SaveLineReq 라인_픽스처(String color, Long distance, String name, Long upStationId, Long downStationId){
+    public static SaveLineReq 라인_생성_픽스처(String color, Long distance, String name, Long upStationId, Long downStationId){
         return new SaveLineReq(color,distance,name,upStationId,downStationId);
     }
 
