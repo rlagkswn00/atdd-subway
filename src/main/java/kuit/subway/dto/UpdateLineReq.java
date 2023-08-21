4ca60481cc58ed79a18a56ac5fbb17cf654618a6
Han Ju Kim <hjkim2902@gmail.com>
2023-08-21 10:54:27 +0900

    feat : 라인 수정 dto 생성

---
+++ src/main/java/kuit/subway/dto/UpdateLineReq.java
@@ -0,0 +1,19 @@
+package kuit.subway.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateLineReq {
+    private String color;
+    private Long distance;
+    private String name;
+    private Long downStationId;
+    private Long upStationId;
+}
