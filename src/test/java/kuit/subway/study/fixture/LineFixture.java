17f58181f4dac87d9a299aaa561c03b4e347210b
Han Ju Kim <hjkim2902@gmail.com>
2023-08-21 10:53:50 +0900

    feat : 지하철 라인 수정 픽스처 생성

--- src/test/java/kuit/subway/study/fixture/LineFixture.java
+++ src/test/java/kuit/subway/study/fixture/LineFixture.java
@@ -2,9 +2,14 @@ package kuit.subway.study.fixture;
 
 import kuit.subway.domain.Line;
 import kuit.subway.dto.SaveLineReq;
+import kuit.subway.dto.UpdateLineReq;
 
 public class LineFixture {
     public static SaveLineReq 라인_픽스처(String color, Long distance, String name, Long upStationId, Long downStationId){
         return new SaveLineReq(color,distance,name,upStationId,downStationId);
+    }
+
+    public static UpdateLineReq 라인_수정_픽스처(String color, Long distance, String name, Long upStationId, Long downStationId){
+        return new UpdateLineReq(color,distance,name,upStationId,downStationId);
     }
 }
