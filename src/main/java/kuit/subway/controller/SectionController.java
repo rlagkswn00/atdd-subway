280e80f428355007c3ec08f90acdb5497c4abc55
Han Ju Kim <hjkim2902@gmail.com>
2023-08-27 15:09:48 +0900

    feat : 구간 삭제 Controller 구현

--- src/main/java/kuit/subway/controller/SectionController.java
+++ src/main/java/kuit/subway/controller/SectionController.java
@@ -5,10 +5,7 @@ import kuit.subway.dto.SaveSectionRes;
 import kuit.subway.service.LineService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -23,6 +20,13 @@ public class SectionController {
     ResponseEntity<SaveSectionRes> createSection(@RequestBody SaveSectionReq saveSectionReq){
         SaveSectionRes saveSectionRes = lineService.createSections(saveSectionReq);
         return ResponseEntity.created(URI.create("/sections/" + saveSectionRes.getId())).body(saveSectionRes);
+
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Long> deleteSection(@PathVariable Long id){
+        Long responseId = lineService.deleteSection(id);
+        return ResponseEntity.ok(responseId);
 
     }
 }
