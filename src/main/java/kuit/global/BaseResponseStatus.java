52f6e3bb73c7d41bfff740bf528ffca42c2ee11d
Han Ju Kim <hjkim2902@gmail.com>
2023-08-27 15:09:55 +0900

    feat : 구간 삭제 간 예외 코드 생성

--- src/main/java/kuit/global/BaseResponseStatus.java
+++ src/main/java/kuit/global/BaseResponseStatus.java
@@ -19,7 +19,8 @@ public enum BaseResponseStatus {
     DUPLICATE_LINE(HttpStatus.BAD_REQUEST, "중복된 라인 입니다."),
     NOT_EXIST_LINE(HttpStatus.BAD_REQUEST, "라인이 존재하지 않습니다."),
     SAME_UP_DOWN_STATION(HttpStatus.BAD_REQUEST, "상행역과 하행역의 ID가 동일합니다."),
-    INVALID_UPSTATION_SECTION(HttpStatus.BAD_REQUEST, "라인의 하행역과 새로운 구간의 상행역이 같지 않습니다.");
+    INVALID_UPSTATION_SECTION(HttpStatus.BAD_REQUEST, "라인의 하행역과 새로운 구간의 상행역이 같지 않습니다."),
+    ONLY_ONE_SECTION(HttpStatus.BAD_REQUEST,"라인에 구간이 하나뿐이라 삭제 할 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
