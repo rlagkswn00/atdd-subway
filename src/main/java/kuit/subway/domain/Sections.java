fe7afd17f5ec253024d0518902090770b905511f
Han Ju Kim <hjkim2902@gmail.com>
2023-08-28 21:49:07 +0900

    refactor : 유효성 검사 메소드 위치 변경

--- src/main/java/kuit/subway/domain/Sections.java
+++ src/main/java/kuit/subway/domain/Sections.java
@@ -3,6 +3,8 @@ package kuit.subway.domain;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.OneToMany;
+import kuit.global.BaseResponseStatus;
+import kuit.global.exception.SubwayException;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,6 +19,10 @@ public class Sections {
     @OneToMany(mappedBy = "line", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
 
+    public void validateDeleteSection(){
+        if(sections.size() == 2)
+            throw new SubwayException(BaseResponseStatus.ONLY_ONE_SECTION);
+    }
     public void addSection(Section section) {
         log.info(section.toString());
         this.sections.add(section);
